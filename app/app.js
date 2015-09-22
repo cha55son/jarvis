15a09849a4656163a20a8a24c98193412f75bb78
Chason Choate <cha55son@gmail.com>
2015-09-21 22:25:23 -0500

    Added JSON schema checking when receiving data.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var bodyParser = require('body-parser')
+var validate = require('jsonschema').validate;
+var witSchema = require('./wit-schema.json');
 var express = require('express');
 var app = express();
 var bot = require('./bot');
@@ -17,7 +19,24 @@ app
     res.send('Jarvis 👨  is ready!');
 })
 .post('/data', function(req, res) {
-    console.log("Intent triggered: %s", req.body.intent);
+    res.setHeader('Content-Type', 'application/json');
+
+    var validObj = validate(req.body, witSchema);
+    if (validObj.errors.length > 0) {
+        console.error("Failed JSON validation");
+        console.error(require('util').inspect(req.body, true, 20));
+        return res.send(JSON.stringify({ error: "Failed JSON validation" }));
+    }
+    var outcomes = req.body;
+    console.log("Received %s outcome(s)", outcomes.length);
+    for (var i = 0; i < outcomes.length; i++) {
+        var outcome = outcomes[i];
+        console.log("Outcome %s: found intent '%s'", i, outcome.intent);
+    }
+
+    res.send(JSON.stringify({ success: true }));
+    // console.log("Intent triggered: %s", req.body.intent);
+    return;
     if (scripts[req.body.intent]) {
         console.log("Triggering script for %s", req.body.intent);
         var dataOrPromise;
